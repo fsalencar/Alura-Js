7afdac22396a120c03121a0162ee04ea8ab8f2da
fsalencar <fsalencar@outlook.com>
2016-11-28 20:20:02 -0200

    Utilizando objetos

--- Projeto/projeto/calcula-imc.js
+++ Projeto/projeto/calcula-imc.js
@@ -1,12 +1,20 @@
-// Arquivo que calcula IMC do usuário
-var peso=20;
-var altura=1.80;
+	// Arquivo que calcula IMC do usuário
+	var pesoText = document.getElementById("peso-2");
+	var alturaText = document.getElementById("altura-2");
 
-//Calcula IMC
-var diferenteDeZero = (altura != 0);
-if (diferenteDeZero) {
-var imc = peso / (altura * altura);
-console.log("Seu imc é de: "+imc);
-} else {
-console.log("Altura deve ser maior que 0!");
+	var paciente = {"peso" : pesoText.textContent,
+					"altura": alturaText.textContent	
+					};
+
+
+	//Calcula IMC
+	var diferenteDeZero = (paciente.altura != 0);
+	if (diferenteDeZero) {
+	var imc = paciente.peso / (paciente.altura * paciente.altura);
+	var imcText = document.getElementById("imc-2");
+	imcText.textContent = imc;
+
+	console.log("Seu imc é de: "+imc);
+	} else {
+	console.log("Altura deve ser maior que 0!");
 }
