25dd3972921f18247a8828592cb5c48eb5409e65
Felipe Alencar <fsalencar@outlook.com>
2016-11-29 08:24:42 -0200

    Commitando alterações

--- Projeto/projeto/calcula-imc.js
+++ Projeto/projeto/calcula-imc.js
@@ -1,11 +1,16 @@
 // Arquivo que calcula IMC do usuário
-var peso=20;
-var altura=1.80;
+var tdPeso = document.getElementById("peso-2");
+var tdAltura = document.getElementById("altura-2")
+
+var peso = tdPeso.textContent;
+var altura= tdAltura.textContent;
 
 //Calcula IMC
 var diferenteDeZero = (altura != 0);
 if (diferenteDeZero) {
 var imc = peso / (altura * altura);
+vat tdImc = document.getElementById("imc-2");
+tdImc.textContent = imc;
 console.log("Seu imc é de: "+imc);
 } else {
 console.log("Altura deve ser maior que 0!");
