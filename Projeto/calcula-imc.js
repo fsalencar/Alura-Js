d79fb4f025d27acbcb45ffc34de9aa2ee7193f16
Felipe Alencar <fsalencar@outlook.com>
2016-11-30 09:49:09 -0200

    Alterações funcionais

--- Projeto/calcula-imc.js
+++ Projeto/calcula-imc.js
@@ -1,21 +1,26 @@
-//calcula-imc.js
+var trsPacientes = document.getElementsByClassName("paciente");
 
-var tdPeso = document.getElementById("peso-2");
-var tdAltura = document.getElementById("altura-2");
+for(var posicaoAtual = 0; posicaoAtual <= trsPacientes.length - 1; posicaoAtual++){
 
-var peso = tdPeso.textContent;
-var altura = tdAltura.textContent;
+    var pacienteTr = trsPacientes[posicaoAtual];
+    
+    var tdNome = pacienteTr.getElementsByClassName("info-nome")[0]; 
+    var tdPeso = pacienteTr.getElementsByClassName("info-peso")[0]; 
+    var tdAltura = pacienteTr.getElementsByClassName("info-altura")[0];
 
-var paciente = {peso : peso, altura : altura};
+    var paciente = { nome : tdNome.textContent, peso : tdPeso.textContent, altura : tdAltura.textContent};
 
-if(paciente.altura != 0){
-    var imc = paciente.peso / (paciente.altura * paciente.altura);
+    if(paciente.altura != 0){
+        var imc = paciente.peso / (paciente.altura * paciente.altura);
 
-	var tdImc = document.getElementById("imc-2"); 
-	tdImc.textContent = imc;
+        var tdImc = pacienteTr.getElementsByClassName("info-imc")[0]; 
+        tdImc.textContent = imc;
 
-    console.log(imc);
-} else{
-    console.log("Não posso executar uma divisão por 0!");
+        console.log(imc); 
+
+    }else{
+
+        console("Não posso dividir por zero!");
+    }
+
 }
-
